dd31eb47cd02569bf65b69400e078a87fb56e923
dalton-teixeira <dalton.silveira@gmail.com>
2016-03-29 10:15:13 -0300

    Update Profile.js

--- test/Data/Profile.js
+++ test/Data/Profile.js
@@ -1,10 +1,5 @@
 var Manager = {
-    managerEmail: 'apptester@avenuecode.com',
-    managerPassword: 'ac4success',
-    employeeEmail: 'user1acdc@avenuecode.com',
-    employeePassword: 'acdcuser1',
-    buddyEmail: 'ac.brazil.superstars@avenuecode.com',
-    buddyPassword: '5up4success'
+
 };
 
 module.exports = Manager;
